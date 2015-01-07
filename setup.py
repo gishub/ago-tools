4366a26567fd57189db0e75736b34fbc90ba7bb0
Evan Caldwell <evan.caldwell@gmail.com>
2015-01-06 21:33:05 -0500

    relocated files and added setup.py for quick install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    description="Administer ArcGIS Online Organizations with Python",
+    license='Apache',
+    url='www.github.com/Esri/ago-tools',
+    name='agoTools',
+    packages=['agoTools'],
+    package_dir={'':''}
+)
